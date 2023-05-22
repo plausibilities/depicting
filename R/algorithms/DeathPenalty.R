450cd3f645e34caaa93ead7ab4ab7caf245c67fb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-22 18:12:59 +0100

    Update R/algorithms/DeathPenalty.R

--- R/algorithms/DeathPenalty.R
+++ R/algorithms/DeathPenalty.R
@@ -17,16 +17,11 @@ DeathPenalty <- function () {
   T <- as.data.frame(x = matrix(data = scan(file = file.path('data', 'deathPenalty.dat')), ncol = 5, byrow = TRUE))
   names(T) <- c('year', 'datepart', 'supporting', 'against', 'mark')
 
+
   # The fraction & percentage of people in favour
   fractions <- T$supporting / (T$supporting + T$against)
   percentages <- 100 * fractions
-
-  # Graphing
-  years <- T$year + (T$datepart - 6)/12
-  ylimits <- c(min(percentages) - 1, max(percentages) + 1)
-
-  plot(x = years, y = percentages, xlab = 'year', ylab = '% in favour of the penalty',
-       ylim = ylimits, pch = 19, frame.plot = FALSE)
+  abscissae <- T$year + (T$datepart - 6)/12
 
 
   # The standard error of the proportions
@@ -34,15 +29,18 @@ DeathPenalty <- function () {
   #   - standard error of an estimate: s.e. = sqrt(p(1 - p)/n), wherein n is the survey size
   standard_error <- sqrt(fractions*(1 - fractions)/1000)
 
+
   # 68% confidence interval, 0.5(1 + 0.68) = 0.84
-  interval_half <- qnorm(p = 0.840, lower.tail = TRUE) * standard_error
-
-
   # ± 1 standard error
+  interval_half <- qnorm(p = 0.840, lower.tail = TRUE) * standard_error
   fraction_boundaries <- fractions +
     matrix(data = interval_half) %*%  matrix(data = c(-1, 1), nrow = 1, ncol = 2)
-
   percentage_boundaries <- 100 * fraction_boundaries
 
 
+  # Graphing
+  ylimits <- c(min(percentages) - 1, max(percentages) + 1)
+  plot(x = abscissae, y = percentages, xlab = 'year', ylab = '% in favour of the penalty',
+       ylim = ylimits, pch = 19, frame.plot = FALSE)
+  
 }
