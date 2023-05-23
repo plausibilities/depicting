ed6ee083206fc9f3516a8b66bb5c952ffe1ff7db
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-23 14:59:11 +0100

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -3,20 +3,25 @@
 # Created by: greyhypotheses
 # Created on: 23/05/2023
 
-library(magrittr)
 
+# The logs directory
 pathstr <- file.path(getwd(), 'notebooks', 'logs')
 
+
+# Ensuring %>% is accessible
+library(magrittr)
+
+
+# The function that ensures (a) the existence of a logs directory, and (b) accessibility of libraries
 main <- function () {
-
   source(file = file.path('notebooks', 'Logs.R'))
-  Logs(pathstr = pathstr)
-
   source(file = file.path('notebooks', 'Libraries.R'))
+  Logs(pathstr = pathstr)
   Libraries(pathstr = pathstr)
 }
 
+
+# Output
 capture.output(main()) %>%
   sink(file = file(file.path(pathstr, 'temporary.log'), open = 'wt'), type = 'output', split = FALSE) %>%
   sink()
-
