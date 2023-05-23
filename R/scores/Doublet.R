c56ef4199c4c03efb0b6d96ac3d746e8745c4883
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-23 21:48:14 +0100

    Add R/scores/Doublet.R

---
+++ R/scores/Doublet.R
@@ -0,0 +1,34 @@
+# Title     : Doublet.R
+# Objective : Test scores and a pair of predictors
+# Created by: greyhypotheses
+# Created on: 23/05/2023
+
+
+
+Doublet <- function () {
+
+  T <- haven::read_dta(file = file.path(getwd(), 'data', 'childTestScore.dta'))
+  T$mom_hs <- as.factor(T$mom_hs)
+
+  initial <- lm(formula = kid_score ~ mom_hs + mom_iq, data = T)
+  T$estimate <- initial$fitted.values
+  summary(object = initial)
+
+  T %>%
+    ggplot(mapping = aes(x = mom_iq, y = kid_score, colour = mom_hs)) +
+    scale_colour_manual(values = c('orange', 'black'), labels = c('no', 'yes')) +
+    geom_point(alpha = 0.65) +
+    geom_abline(aes(slope = coef(initial)[['mom_iq']],
+                    intercept = coef(initial)[['(Intercept)']] + as.numeric(as.character(mom_hs))*coef(initial)[['mom_hs1']],
+                    colour = mom_hs)
+                ) +
+    theme_minimal() +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(linewidth = 0.05),
+          axis.title.x = element_text(size = 13, face = 'bold'), axis.text.x = element_text(size = 11),
+          axis.title.y = element_text(size = 13, face = 'bold'), axis.text.y = element_text(size = 11)) +
+    xlab(label = "\nmother's intelligence quotient\n") +
+    ylab(label = '\nchild test score\n') +
+    guides(col = guide_legend(title = 'Mother completed\nhigh school?'))
+
+}
