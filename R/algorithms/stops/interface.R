1270f30812ca315d9d4c410c6ad4d8bf6e3d123f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-25 21:37:08 +0100

    Update R/algorithms/stops/interface.R

--- R/algorithms/stops/interface.R
+++ R/algorithms/stops/interface.R
@@ -4,8 +4,10 @@
 # Created on: 25/05/2023
 
 
+# functions
 source(file = file.path(getwd(), 'R', 'algorithms', 'stops', 'StudyData.R'))
 
+# reading-in
 collection <- StudyData()
 fundamental <- collection$fundamental
 head(fundamental)
