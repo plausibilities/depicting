a32e47eccc81a92f053d2556eb0fb203337e2d69
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-25 21:23:53 +0100

    Update R/algorithms/stops/interface.R

--- R/algorithms/stops/interface.R
+++ R/algorithms/stops/interface.R
@@ -6,13 +6,16 @@
 
 source(file = file.path(getwd(), 'R', 'algorithms', 'stops', 'StudyData.R'))
 
-T <- StudyData()
-head(T)
+collection <- StudyData()
+instances <- collection$instances
+head(instances)
 
-# keys-> population, precinct, ethinicity
-# names from -> crime
-# values from -> stops, past.arrests
-pivots <- tidyr::pivot_wider(data = T, names_from = crime, values_from = c(stops, 'past_arrests') )
-head(pivots)
+
+
+
+
+
+
+
 
 
