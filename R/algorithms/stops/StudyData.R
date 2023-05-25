f69c31ec4c1b1b7a7921e9c9cb7c3f2579f38552
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-25 21:23:42 +0100

    Update R/algorithms/stops/StudyData.R

--- R/algorithms/stops/StudyData.R
+++ R/algorithms/stops/StudyData.R
@@ -6,12 +6,45 @@
 
 StudyData <- function () {
 
+  # The data
   T <- read.table(file = file.path(getwd(), 'data', 'policeStops.dat'), header = TRUE, skip = 6)
   T <- dplyr::rename(T, population = 'pop', ethnicity = 'eth', 'past_arrests' = 'past.arrests')
 
+
+  # Factoring
   T$ethnicity <- factor(x = T$ethnicity, levels = c(1, 2, 3), labels = c('black', 'hispanic', 'white'))
   T$crime <- factor(x = T$crime, levels = c(1, 2, 3, 4), c('violent', 'weapons', 'property', 'drug'))
 
-  return(T)
+
+  # keys-> population, precinct, ethinicity
+  # names from -> crime
+  # values from -> stops, past.arrests
+  T %>%
+    tidyr::pivot_wider(names_from = crime, values_from = c(stops, 'past_arrests') )
+
+
+  # keys-> population, precinct, ethinicity
+  # names from -> crime
+  # values from -> stops
+  T %>%
+    select(!past_arrests) %>%
+    tidyr::pivot_wider(names_from = crime, values_from = stops)
+
+
+  # keys-> population, precinct, ethinicity
+  # names from -> crime
+  # values from -> past_arrests
+  T %>%
+    select(!stops) %>%
+    tidyr::pivot_wider(names_from = crime, values_from = 'past_arrests')
+
+
+  # Aggregated by stops & past arrests
+  instances <- T %>%
+    group_by(precinct, ethnicity, population) %>%
+    summarise(across(c(stops, past_arrests), sum))
+
+
+  return(list(T = T, instances = instances))
 
 }
