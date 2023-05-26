1fa7b09695c0dfe6c44e3b0da59b8f191fd9b716
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-26 23:12:12 +0100

    Add R/functions/StandardisedResidual.R

---
+++ R/functions/StandardisedResidual.R
@@ -0,0 +1,18 @@
+# Title     : StandardisedResidual.R
+# Objective : Standardised Residual
+# Created by: greyhypotheses
+# Created on: 26/05/2023
+
+
+#' The standardised residual
+#'
+#' @param y: The dependent variable.
+#' @param estimates: The dependent variable estimates, courtesy of a model.
+#'
+StandardisedResidual <- function (y, estimates) {
+
+  z <- (y - estimates)/sqrt(estimates)
+
+  return(z)
+
+}
