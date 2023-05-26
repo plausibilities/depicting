15824560e87b06b2c9e67b7c465ea26f4a16c671
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-26 23:11:46 +0100

    Add R/algorithms/stops/GLM.R

---
+++ R/algorithms/stops/GLM.R
@@ -0,0 +1,23 @@
+# Title     : GLM.R
+# Objective : Generalised Linear Model
+# Created by: greyhypotheses
+# Created on: 26/05/2023
+
+
+#' Generalised Linear Model
+#'
+#' @param data
+#' @param type
+#' 
+GLM <- function(data, type) {
+
+  base::switch(type,
+               simple = glm(formula = stops ~ 1, family = poisson(link = 'log'),
+                            offset = log(x = arrests), data = data),
+               extended = glm(formula = stops ~ ethnicity, family = poisson(link = 'log'),
+                              offset = log(x = arrests), data = data),
+               core = glm(formula = stops ~ ethnicity + precinct, family = poisson(link = 'log'),
+                          offset = log(x = arrests), data = data)
+  )
+
+}
