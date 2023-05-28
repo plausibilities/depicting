0f95d463142709526b79db3dcfa910673ae82fd8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-28 15:19:51 +0100

    Update R/stops/interface.R

--- R/stops/interface.R
+++ R/stops/interface.R
@@ -16,8 +16,9 @@ fundamental <- collection$fundamental
 head(fundamental)
 
 
+# Note, the the value of the dispersion parameter vis-a-vis the quasi model (quasipoisson family) is
+# equivalent to the calculated overdispersion ratio of the core model (poisson family)
 core <- Execute(data = fundamental, type = 'core')
-
 quasi <- Execute(data = fundamental, type = 'quasi')
 
 
