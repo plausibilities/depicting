e978d0727250d44573d6c25ce914cdc7fc1dbaef
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-28 13:55:58 +0100

    Add R/stops/modelling/Execute.R

---
+++ R/stops/modelling/Execute.R
@@ -0,0 +1,40 @@
+# Title     : Execute.R
+# Objective : Execute
+# Created by: greyhypotheses
+# Created on: 28/05/2023
+
+
+source(file = file.path(getwd(), 'R', 'stops', 'GLM.R'))
+source(file = file.path(getwd(), 'R', 'algorithms', 'StandardisedResidual.R'))
+
+
+Execute <- function(data, type) {
+
+  # A generalised linear model; model is a stats::glm(.) model object
+  model <- GLM(data = data, type = type)
+
+
+  # Estimate
+  diagnostics <- cbind(data, estimate = as.numeric(stats::predict.glm(object = model, type = 'response')))
+
+
+  # raw residual
+  diagnostics$residual_raw <- diagnostics$stops - diagnostics$estimate
+  raw <- plot(x = diagnostics$estimate, y = diagnostics$residual_raw, frame.plot = FALSE,
+              xlab = 'prediction', ylab = 'residual')
+
+
+  # standardised residual
+  diagnostics$residual_standardised <- StandardisedResidual(y = diagnostics$stops, estimates = diagnostics$estimate)
+  standardised <- plot(x = diagnostics$estimate, y = diagnostics$residual_standardised, frame.plot = FALSE,
+                      xlab = 'prediction', ylab = 'standardised residual')
+
+
+  # overdispersion test; reject the null hypothesis - overdispersion ≈ 0 - at the 0.05 significance level
+  # the degree of freedom, i.e., df, is number of observations - number of model parameters
+  pchisq(q = sum(`^`(diagnostics$residual_standardised, 2)),
+         df = model$df.residual,
+         lower.tail = TRUE)
+
+
+}
