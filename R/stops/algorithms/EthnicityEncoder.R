2fbe5d47348293fcfcaf94926ff4bb380db84212
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-29 20:27:03 +0100

    Update R/stops/algorithms/EthnicityEncoder.R

--- R/stops/algorithms/EthnicityEncoder.R
+++ R/stops/algorithms/EthnicityEncoder.R
@@ -9,26 +9,26 @@
 #' @param blob: The dataframe hosting the ethnicity fields of interesting
 #' @param fields: The ethnicity fields to be encoded
 #'
-EthnicityEncoder <- function (blob, fields) {
+EthnicityEncoder <- function(blob, fields) {
 
   # The encoding function; if the field value is NA, zero is
   # assigned, otherwise 1 is assigned.
-  temporary <- function (x) {
+  temporary <- function(x) {
 
     y <- case_when(
       is.na(x) ~ 0,
       TRUE ~ 1)
     y <- factor(y, levels = c(0, 1))
-    
+
     return(y)
   }
-  
+
   # Encode the fields in question
   K <- lapply(X = blob[, fields], FUN = temporary)
-  
+
   # Replace the original fields with their encoded forms
   blob[, fields] <- dplyr::bind_cols(K)
-  
+
 
   return(blob)
 
