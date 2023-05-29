11011a7185a4cb1ee4a997a65d9d2d48137f693f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-29 18:09:23 +0100

    Add R/stops/algorithms/EthnicityEncoder.R

---
+++ R/stops/algorithms/EthnicityEncoder.R
@@ -0,0 +1,31 @@
+# Title     : EthnicityEncoder.R
+# Objective : Ethicity Encoder
+# Created by: greyhypotheses
+# Created on: 29/05/2023
+
+
+
+EthnicityEncoder <- function (blob, fields) {
+
+  # The encoding function; if the field value is NA, zero is
+  # assigned, otherwise 1 is assigned.
+  temporary <- function (x) {
+
+    y <- case_when(
+      is.na(x) ~ 0,
+      TRUE ~ 1)
+    y <- factor(y, levels = c(0, 1))
+    
+    return(y)
+  }
+  
+  # Encode the fields in question
+  K <- lapply(X = blob[, fields], FUN = temporary)
+  
+  # Replace the original fields with their encoded forms
+  blob[, fields] <- dplyr::bind_cols(K)
+  
+
+  return(blob)
+
+}
