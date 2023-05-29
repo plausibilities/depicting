3b799010871af6521b90f5474edd859e23f7850f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-05-29 20:10:53 +0100

    Update R/stops/interface.R

--- R/stops/interface.R
+++ R/stops/interface.R
@@ -12,22 +12,22 @@ source(file = file.path(pathstr, 'modelling', 'Execute.R'))
 
 # reading-in
 collection <- StudyData()
-fundamental <- collection$fundamental
-head(fundamental)
+aggregated <- collection$aggregated
+head(aggregated)
 
 
 # Note, the the value of the dispersion parameter vis-a-vis the quasi model (quasipoisson family) is
 # equivalent to the calculated overdispersion ratio of the core model (poisson family)
-core <- Execute(data = fundamental, model.name = 'core')
-quasi <- Execute(data = fundamental, model.name = 'quasi')
+core <- Execute(data = aggregated, model.name = 'core')
+quasi <- Execute(data = aggregated, model.name = 'quasi')
 
 
 # Experiment
 model <- glmer(formula = stops ~ 1 + ethnicity + (0 + ethnicity|precinct),
-               data = fundamental, family = poisson(), offset = log(x = arrests), nAGQ = 1)
+               data = aggregated, family = poisson(), offset = log(x = arrests), nAGQ = 1)
 
 Q <- glmer(formula = stops ~ 1 + ethnicity + (1 + ethnicity|precinct),
-               data = fundamental, family = poisson(), offset = log(x = arrests), nAGQ = 1)
+               data = aggregated, family = poisson(), offset = log(x = arrests), nAGQ = 1)
 
 negative <- glmer.nb(formula = stops ~ 1 + ethnicity + (0 + ethnicity|precinct),
-                  data = fundamental, offset = log(x = arrests), nAGQ = 1)
+                  data = aggregated, offset = log(x = arrests), nAGQ = 1)
